8e1875aeee5b9512eb56713af2fa750162cc81ca
Caires <pedroachiliferreira@gmail.com>
2026-06-10 11:17:44 -0300

    coletaveis

--- primeiro-jogo/Scenes/inimigo.gd
+++ primeiro-jogo/Scenes/inimigo.gd
@@ -5,33 +5,32 @@ const GRAVITY = 800.0
 
 var direction = 1
 
-
-@onready var floor_left: Raycast2D = $FloorLeft 
-
-# nome do nó
-@onready var floor_right: RayCast2D = $FloorRight
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 @onready var left_limit: Marker2D = $"../left_limit"
 @onready var right_limit: Marker2D = $"../right_limit"
 
 func _physics_process(delta):
-# Garante que a gravidade seja aplicada ao inimigo caso ele não esteja no chão
+	# Aplica gravidade
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
-# Inverte ao detectar borda com o método padrão do Raycast2D is_colliding()
-	if position.x >= right_limit.global_position.x:
+
+	# Verifica os limites
+	if global_position.x >= right_limit.global_position.x:
 		direction = -1
-	if position.x <= left_limit.global_position.x:
+
+	if global_position.x <= left_limit.global_position.x:
 		direction = 1
-# Aplica velocidade no eixo x
+
+	# Movimento horizontal
 	velocity.x = direction * SPEED
-# Vira o sprite do personagem se estiver indo para a direita
+
+	# Vira o sprite
 	anim.flip_h = direction > 0
-# Roda a animação de caminhar
-	anim.play("walk")
-	
-func _on_timer_timeout() -> void:
-	direction *= -1
-# Move o inimigo
-		move_and_slide()
+
+	# Toca animação
+	if anim.animation != "walk":
+		anim.play("walk")
+
+	# Move o personagem
+	move_and_slide()
---
+++ primeiro-jogo/coin.gd
@@ -0,0 +1,36 @@
+extends Area2D
+# Novo sinal criado (é possível criar um sinal caso os padrões não
+# tenham o que é preciso
+signal collected
+# Cria as variáveis que referenciam o nó de Partículas, Sprite2D e CollisionShape
+@onready var particles: GPUParticles2D = $Particles
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+# Ao ser criado na tela, o sinal body_entered (sinal emitido automaticamente
+# quando um corpo físico entra na área do nó Area2D) é conectado à função
+# on_body_entered
+func _ready():
+	body_entered.connect(_on_body_entered)
+# Toda vez que um corpo entra na Area2D, a função _on_body_entered recebe
+# automaticamente como parâmetro o nó que entrou nessa área, representado
+# pelo bodyApostila — Coletáveis e Power-ups4
+func _on_body_entered(body):
+# Se o nome do nó que entrou na área for Player, execute as ações
+		if body.name == "player":
+# printe no console "+1"
+			print("+1")
+# Emita o sinal collected (criado no início do código)
+			collected.emit()
+# Deixa a moeda invisível
+			sprite_2d.visible = false
+# Desabilita o colisor (set_deferred é um modo mais seguro do que acessar
+# diretamente a propriedade disabled
+			collision_shape_2d.set_deferred("disabled", true)
+# Ativa as partículas
+			particles.emitting = true
+# Espera as partículas acabarem antes de seguir
+# O await pausa a execução da função até que o sinal finished seja emitido
+# — sem travar o jogo, pausando somente a função de forma assíncrona.
+			await particles.finished
+# Remova a moeda da cena
+			queue_free()
